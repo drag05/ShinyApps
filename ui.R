792d40eb993a1b390f8bb7f652d9c27a4afd6796
Dragos Bandur <39677929+drag05@users.noreply.github.com>
2020-10-12 12:32:29 -0400

    Update ui.R
    Inserted message suppression

--- ui.R
+++ ui.R
@@ -13,6 +13,8 @@ shinyUI(
     fluidPage(suppress_bootstrap = TRUE,
 
       tags$style(type = 'text/css'
+                   ,".shiny-output-error { visibility: hidden; }"
+                   ,".shiny-output-error:before { visibility: hidden; }"
                    , 'html
                    , body {width:100%;height:100%}')
                    , leafletOutput('BalticSea'
